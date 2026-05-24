c26d6815b61e14d46f905a37d29f74c914bc9931
simranoble04-bloom <simranoble04@gmail.com>
2026-05-24 11:37:31 +0530

    Update day_1.cpp

--- day_1.cpp
+++ day_1.cpp
@@ -1,14 +1,44 @@
 #include<iostream>
 using namespace std;
 int main() {
-    // Write a program to Calculate sum of first N natural numbers.
+    // 1. Write a program to Calculate sum of first N natural numbers.
     int n, sum=0;
     cout<<"Enter n: ";
     cin>>n;
     for(int i=1; i<=n; i++){
         sum+=i;
     }
-    cout<<"sum: "<<sum;
+    cout<<"sum: "<<sum<<endl;
+
+    // 2. Write a program to Print multiplication table of a given number.
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=1; i<=10; i++){
+        cout<<i<<" * "<<n<<" = "<<(i*n)<<endl;
+    }
+
+    //3. Write a program to Find factorial of a number.
+    int n, fact=1;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        fact*=i;
+    }
+    cout<<"Factorial of "<<n<<" is: "<<fact<<endl;
+
+    //4. Write a program to Count digits in a number.
+    int n, dig=0, n_cpy;
+    cout<<"Enter n: ";
+    cin>>n;
+    n_cpy=n;
+    while (n_cpy>0){
+        n_cpy/=10;
+        dig++;
+    }
+    cout<<"The number of digits in "<<n<<" is: "<<dig<<endl;
+
+
     
     return 0;
 }
