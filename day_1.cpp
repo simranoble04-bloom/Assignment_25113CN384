4fc90a1cfcb77d05f42cedc81b8a4c68f611048e
simranoble04-bloom <simranoble04@gmail.com>
2026-05-24 11:16:09 +0530

    Implement sum of first N natural numbers
    Fix the sum calculation loop to increment i.

--- day_1.cpp
+++ day_1.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 int main() {
-    int a=2;
+    // Write a program to Calculate sum of first N natural numbers.
+    int n, sum=0;
+    cout<<"Enter n: ";
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        sum+=i;
+    }
+    cout<<"sum: "<<sum;
+    
     return 0;
 }
