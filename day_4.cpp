9e0e6472784f9fa709d9d38a3c5f63f0bc8b104f
simranoble04-bloom <simranoble04@gmail.com>
2026-05-25 10:12:01 +0530

    Initialize Fibonacci series program in day_4.cpp
    Added a skeleton for a Fibonacci series program.

---
+++ day_4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //13. Write a program to Generate Fibonacci series.
+    int n1;
+    cout<<"Enter n1: ";
+    cin>>n1;
+
+
+    
+    return 0;
+}
