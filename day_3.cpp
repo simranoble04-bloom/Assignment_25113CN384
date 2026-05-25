aaf9f1d9f6f526707cd5a56169db4c9156ad23cd
simranoble04-bloom <simranoble04@gmail.com>
2026-05-25 09:06:10 +0530

    Create day_3.cpp
    Added a program skeleton to check for prime numbers.

---
+++ day_3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    //9. Write a program to Check whether a number is prime.
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    
+
+    return 0;
+}
